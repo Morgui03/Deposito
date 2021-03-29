c1eae7e61d11abfcd1672e22e6fbf2eb7397444d
Morgana <Morgana@192.168.1.131>
2021-03-29 03:26:16 +0200

    Test

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+	
+		DepositoCombustible depositoT = new DepositoCombustible(10.0,5.2);
+		
+	@Test
+	public void testGetDepositoNivel() {
+		assertEquals(5.2,depositoT.getDepositoNivel(),0);
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		assertEquals(10.0,depositoT.getDepositoMax(),0);
+	}
+
+	@Test
+	public void testEstaVacio() {
+		assertFalse(depositoT.estaVacio());
+	}
+
+	@Test
+	public void testEstaLleno() {
+		assertFalse(depositoT.estaLleno());
+	}
+
+	@Test
+	public void testConsumir() {
+		depositoT.consumir(3);
+			assertEquals(2.2, depositoT.getDepositoNivel(),0);
+		depositoT.consumir(-3);
+			assertEquals(5.2, depositoT.getDepositoNivel(),0);
+	}
+
+}
